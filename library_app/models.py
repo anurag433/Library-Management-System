44351db36892bf1198bf08e45cc5ea90fbb7f999
Anurag Singh <anurag504singh@gmail.com>
2025-04-27 19:24:24 +0530

    first commit

---
+++ library_app/models.py
@@ -0,0 +1,15 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=100)
+#     author = models.CharField(max_length=100)
+#    # quantity = models.IntegerField(max_length=10)
+#     available = models.BooleanField(default=True)
+
+
+# class BorrowRequest(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     book = models.ForeignKey(Book, on_delete=models.CASCADE) 
+#     request_date = models.DateField()
+#     is_approved = models.BooleanField()
