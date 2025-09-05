52a19689d8598336ef9d2a4b7e60cf9a81b75654
Anurag Singh <anurag504singh@gmail.com>
2025-09-05 16:21:50 +0530

    Fix Vercel deployment config

--- library_project/wsgi.py
+++ library_project/wsgi.py
@@ -1,7 +1,7 @@
-# api/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_project.settings")
 
-app = get_wsgi_application()  
+application = get_wsgi_application()
+app = application  
