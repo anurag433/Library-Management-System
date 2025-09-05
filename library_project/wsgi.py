09ca7cb0a32bcfb62fd4627cae3cc4f3dd60bafc
Anurag Singh <anurag504singh@gmail.com>
2025-09-05 16:13:24 +0530

    deployment

--- library_project/wsgi.py
+++ library_project/wsgi.py
@@ -2,5 +2,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_project.settings")
+
 app = get_wsgi_application()  
